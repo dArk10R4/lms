20242a3500f5f4a4ed555ea31b3314423aef5af5
dArk10R4 <goylerov04@gmail.com>
2023-12-31 10:37:23 +0400

    show body

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const http = require('http');   
 const app = express(); 
 
+app.use(express.json());
 
 app.get('/', function (req, res) {
    console.log('Hello World!');
     res.send('Hello World!');
 });
 
-http.createServer(app).listen(3000, function () {
-    console.log('Listening on port 3000');
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.status(200).send();
+});
+
+const port = 443;
+
+http.createServer(app).listen(port, function () {
+    console.log('Listening on port ' + port);
 });
